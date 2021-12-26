09a7b1f5bef9d713f087253609ae894e3a2aa84a
masterasia1 <a.mayfield18@gmail.com>
2021-12-26 14:51:54 -0800

    work

--- api/task/model.js
+++ api/task/model.js
@@ -1,3 +1,18 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
 
+module.exports = {
+  find,
+ //add,
+ }
+
+ function find() { 
+  return db('tasks as t')
+    .leftJoin('projects as p', 'p.project_id', 't.project_id')
+    .select('t.task_id','p.project_name','p.project_description');
+}
+
+
+  //[ ] `[GET] /api/tasks`
+ // - Even though `task_completed` is stored as an integer, the API uses booleans when interacting with the client
+ // - Each task must include `project_name` and `project_description`
--- api/task/router.js
+++ api/task/router.js
@@ -4,5 +4,16 @@ const Task = require('./model')
 
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+    Task.find()
+      .then(tasks => {
+        res.json(tasks)
+      })
+      .catch(next)
+  })
+
+//[ ] `[GET] /api/tasks`
+ // - Even though `task_completed` is stored as an integer, the API uses booleans when interacting with the client
+ // - Each task must include `project_name` and `project_description`
 
 module.exports = router
