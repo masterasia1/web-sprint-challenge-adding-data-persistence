82a96fe0f18c2a07efe17d4daeb5edbdaf64824b
masterasia1 <a.mayfield18@gmail.com>
2021-12-25 23:33:56 -0800

    workin

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const ProjectRouter = require('./project/router')
+const ResourceRouter = require('./resource/router')
+const TaskRouter = require('./task/router')
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/project', ProjectRouter)
+server.use('/api/resource', ResourceRouter)
+server.use('/api/task', TaskRouter)
+
+
+//ffff
+module.exports = server;
